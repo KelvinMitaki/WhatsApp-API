607ee7b064a1a4e8b396e9a58b930ce8c69440cb
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-28 18:49:27 +0300

    wired up connection to the database

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import { ApolloServer } from "apollo-server";
+import mongoose from "mongoose";
 import resolvers from "./resolvers";
 import typeDefs from "./schema/schema";
 const server = new ApolloServer({
   resolvers,
   typeDefs
 });
+
+if (!process.env.MONGO_URI) {
+  throw new Error("Mongo uri is required");
+}
+
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectMongo();
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at url ${url}`);
