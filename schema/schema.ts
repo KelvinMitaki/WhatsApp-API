d8f809f5da1dcd7550070ef913459a588589adee
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-29 15:17:13 +0300

    wired up message user and group typedefs

--- schema/schema.ts
+++ schema/schema.ts
@@ -5,6 +5,27 @@ const typeDefs = gql`
   type Hello {
     world: String!
   }
+  type User {
+    name: String!
+    about: String!
+    phoneNumber: Int!
+    countryCode: String!
+    profilePhoto: String
+    groups: [String]!
+  }
+  type Message {
+    sender: String!
+    recipient: String!
+    message: String!
+    read: Boolean!
+  }
+  type Group {
+    name: String!
+    description: String
+    groupProfilePhoto: String
+    admin: String!
+  }
+
   type Query {
     hello: Hello!
   }
