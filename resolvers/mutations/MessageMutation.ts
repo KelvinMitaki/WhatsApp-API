661b15dd127bb459bfea395bae1df6b8a9dae659
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-29 21:47:26 +0300

    added addNewMessage mutation

--- resolvers/mutations/MessageMutation.ts
+++ resolvers/mutations/MessageMutation.ts
@@ -1,7 +1,12 @@
+import { auth } from "../../middlewares/UserValidation";
+import { Message } from "../../models/Message";
 import { Resolver } from "../queries/UserQuery";
 
 export const MessageMutation: Resolver = {
-  addNewMessage(prt, args: { recipient: string; message: string }, { req }) {
-    return {};
+  async addNewMessage(prt, args: { recipient: string; message: string }, { req }) {
+    const id = auth(req) as string;
+    const message = Message.build({ ...args, sender: id, read: false });
+    await message.save();
+    return message;
   }
 };
