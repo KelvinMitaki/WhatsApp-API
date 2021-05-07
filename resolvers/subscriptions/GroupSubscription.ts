d3d9dd8f4efbf8b693a9e5c563e481444d047bd4
KelvinMitaki <mitakikelvin1@gmail.com>
2021-05-07 15:35:13 +0300

    fixed ADD_NEW_GROUP subscription bug

--- resolvers/subscriptions/GroupSubscription.ts
+++ resolvers/subscriptions/GroupSubscription.ts
@@ -9,12 +9,10 @@ export const GroupSubscription: Subscription = {
       () => pubsub.asyncIterator(SubscriptionEnum.ADD_NEW_GROUP),
       (payload, variables: { userID: string }) => {
         const { participants, admin }: GroupDoc = payload.addNewGroup;
-        console.log(payload);
-        const broadcast =
+        return (
           participants.some(p => p.toString() === variables.userID.toString()) ||
-          admin.toString() === variables.userID.toString();
-        console.log({ broadcast });
-        return broadcast;
+          admin.toString() === variables.userID.toString()
+        );
       }
     )
   },
