91ee8d62367210882bc1fd1a902e1d7aa59e48c8
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-13 13:46:11 +0300

    wired up UserQuery typings

--- resolvers/queries/UserQuery.ts
+++ resolvers/queries/UserQuery.ts
@@ -1,37 +1,25 @@
-import { auth } from "../../middlewares/UserValidation";
-import { User } from "../../models/User";
-import { Context } from "../../schema/schema";
-import { pubsub, SubscriptionEnum } from "../subscriptions/MessageSubscription";
+import { QueryResolvers, ResolverTypeWrapper, User as UserInterface } from '../../generated/graphql'
+import { auth } from '../../middlewares/UserValidation'
+import { User } from '../../models/User'
+import { pubsub, SubscriptionEnum } from '../subscriptions/MessageSubscription'
 
-export type Resolver = {
-  [key: string]: (
-    prt: any,
-    args: any,
-    context: Context
-  ) =>
-    | { [key: string]: any }
-    | Promise<{ [key: string]: any }>
-    | Promise<{ [key: string]: any }[]>
-    | null;
-};
-
-export const UserQuery: Resolver = {
+export const UserQuery: QueryResolvers = {
   async fetchCurrentUser(prt, args, { req }) {
-    const id = auth(req);
-    const user = await User.findById(id).populate("groups");
+    const id = auth(req)
+    const user = await User.findById(id).populate('groups')
     if (user) {
-      user.online = true;
-      user.lastSeen = new Date().toString();
-      await user.save();
+      user.online = true
+      user.lastSeen = new Date().toString()
+      await user.save()
     }
-    const data = { userID: id, online: true };
-    pubsub.publish(SubscriptionEnum.UPDATE_USER_ONLINE, { updateUserOnline: data });
-    return user;
+    const data = { userID: id, online: true }
+    pubsub.publish(SubscriptionEnum.UPDATE_USER_ONLINE, { updateUserOnline: data })
+    return user as unknown as UserInterface
   },
   fetchUsers(prt, args, { req }) {
-    const id = auth(req);
+    const id = auth(req)
     return User.find({ _id: { $ne: id } })
-      .populate("groups")
-      .limit(10);
-  }
-};
+      .populate('groups')
+      .limit(10) as unknown as ResolverTypeWrapper<UserInterface>[]
+  },
+}
