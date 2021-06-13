a93930a6e3cfb8ede844010246b038a95b68edb5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-13 13:14:37 +0300

    fixed GroupMutation typings

--- resolvers/queries/GroupQuery.ts
+++ resolvers/queries/GroupQuery.ts
@@ -1,84 +1,84 @@
-import mongoose from "mongoose";
-import { ForbiddenError } from "apollo-server-errors";
-import { auth } from "../../middlewares/UserValidation";
-import { Group } from "../../models/Group";
-import { GroupMsg } from "../../models/GroupMsg";
-import { Resolver } from "./UserQuery";
+import mongoose from 'mongoose'
+import { ForbiddenError } from 'apollo-server-errors'
+import { auth } from '../../middlewares/UserValidation'
+import { Group, GroupDoc } from '../../models/Group'
+import { GroupMsg } from '../../models/GroupMsg'
+import { Resolver } from './UserQuery'
 
 export const GroupQuery: Resolver = {
   async fetchGroups(prt, args, { req }) {
-    const id = auth(req);
+    const id = auth(req)
     const groups = await Group.find({ $or: [{ participants: id }, { admin: id }] })
       .populate({
-        path: "message",
+        path: 'message',
         populate: {
-          path: "sender"
-        }
+          path: 'sender',
+        },
       })
       .sort({ updatedAt: -1 })
-      .limit(10);
-    return groups;
+      .limit(10)
+    return groups
   },
   async fetchGroup(prt, args: { groupID: string }, { req }) {
-    const id = auth(req);
+    const id = auth(req)
     const group = await Group.findOne({ _id: args.groupID, participants: id }).populate(
-      "participants"
-    );
+      'participants'
+    )
     if (!group) {
-      throw new ForbiddenError("You are not allowed to view this group's info");
+      throw new ForbiddenError("You are not allowed to view this group's info")
     }
-    return group;
+    return group
   },
   async fetchGroupMsgs(
     prt,
     args: { groupID: string; offset: number; limit: number; messageCount: number },
     { req }
   ) {
-    const id = auth(req);
+    const id = auth(req)
     const isParticipant = await Group.exists({
       _id: args.groupID,
-      $or: [{ participants: id }, { admin: id }]
-    });
+      $or: [{ participants: id }, { admin: id }],
+    })
     if (!isParticipant) {
-      throw new ForbiddenError("You are not allowed to read messages from this group");
+      throw new ForbiddenError('You are not allowed to read messages from this group')
     }
-    const skip = args.messageCount - (args.offset + args.limit);
+    const skip = args.messageCount - (args.offset + args.limit)
     return GroupMsg.find({ group: args.groupID })
-      .populate("sender")
+      .populate('sender')
       .skip(skip < 0 ? 0 : skip)
-      .limit(args.limit);
+      .limit(args.limit)
   },
   async fetchUnreadGroupMsgs(prt, args, { req }) {
-    const id = auth(req);
-    let groups = await Group.find({ participants: id }).select({ _id: 1 });
-    groups = groups.map(g => g._id);
+    const id = auth(req)
+    let groups = await Group.find({ participants: id }).select({ _id: 1 })
+    groups = groups.map((g) => g._id) as unknown as GroupDoc[]
     return GroupMsg.aggregate([
       {
         $match: {
           group: { $in: groups },
           read: { $nin: [mongoose.Types.ObjectId(id)] },
-          sender: { $ne: mongoose.Types.ObjectId(id) }
-        }
+          sender: { $ne: mongoose.Types.ObjectId(id) },
+        },
       },
-      { $group: { _id: { group: "$group" }, messageCount: { $sum: 1 } } },
-      { $project: { group: "$_id.group", _id: 0, messageCount: 1 } }
-    ]);
+      { $group: { _id: { group: '$group' }, messageCount: { $sum: 1 } } },
+      { $project: { group: '$_id.group', _id: 0, messageCount: 1 } },
+    ])
   },
   async fetchGroupMessageCount(prt, args: { groupID: string }, { req }) {
-    auth(req);
+    auth(req)
     return {
-      count: await GroupMsg.countDocuments({ group: args.groupID })
-    };
+      count: await GroupMsg.countDocuments({ group: args.groupID }),
+    }
   },
   async fetchGroupMessagesCount(_, __, { req }) {
-    const id = auth(req);
-    const groups = await Group.find({ participants: id }).select("_id");
+    const id = auth(req)
+    const groups = await Group.find({ participants: id }).select('_id')
     return GroupMsg.aggregate([
-      { $match: { group: { $in: groups.map(g => g._id) } } },
+      { $match: { group: { $in: groups.map((g) => g._id) } } },
       {
-        $group: { _id: { groupID: "$group" }, messageCount: { $sum: 1 } }
+        $group: { _id: { groupID: '$group' }, messageCount: { $sum: 1 } },
       },
-      { $project: { groupID: "$_id.groupID", _id: 0, messageCount: 1 } }
-    ]);
-  }
-};
+      { $project: { groupID: '$_id.groupID', _id: 0, messageCount: 1 } },
+    ])
+  },
+}
